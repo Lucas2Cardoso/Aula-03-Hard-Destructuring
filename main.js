16dc701743fed7fce3e1005f754540b222fec0b8
Lucas2Cardoso <lucascardoso1302@gmail.com>
2022-10-28 11:29:06 -0300

    Destructuring

---
+++ main.js
@@ -0,0 +1,58 @@
+// Exercício 1 
+
+// const alunos = ['Alice', 'Gabriela', 'João', 'Francisco', 'Natália', 'Giovana', 'Gabriel']
+
+// const [Aluno1, Aluno2, Aluno3, Aluno4, Aluno5, Aluno6, Aluno7] = alunos;
+
+// console.log(Aluno3);
+
+
+
+// Exercício 2 
+
+// const disciplinas = ['Matemática', 'Física', 'Química', 'LínguaPortuguesa', 'Biologia']
+
+// const [Materia1, Materia2, , Materia4, Materia5] = disciplinas;
+
+// console.log(Materia1);
+
+
+
+// Exercícios 3 
+
+// const dadosCliente = {
+//     nome: 'Fabio',
+//     idade: 45,
+//     endereco: {
+//         logradouro: 'Rua das Palmeiras',
+//         numero: 42,
+//         bairro: 'Barra da Tijuca',
+//         cidade: 'Rio de Janeiro'
+//     },
+//     assinanteDesde: 2017
+// }
+
+// const { nome, idade, endereco:{ logradouro, numero, bairro, cidade } = endereco, assinanteDesde } = dadosCliente;
+
+// console.log(bairro);
+
+
+
+// Exercícios 4
+
+// const vestimenta = {
+//     camisa: {
+//         listrada: true,
+//         corCamisa: 'Vermelha e branca',
+//         colecaoCamisa: 'Outono/Inverno 2019'
+//     },
+//     calca: {
+//         corCalca: 'Azul escuro',
+//         colecaoCalca: 'Primavera/Verão 2021'
+//     }
+// }
+
+// const { camisa: { listrada, corCamisa, colecaoCamisa } = camisa, calca: { corCalca, colecaoCalca} = calca } = vestimenta;
+
+// console.log(corCalca);
+
